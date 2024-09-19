c6ab9aa3d7904892b11b1eed01833983e6902d34
mohammed.umer <mohammed.umer@ivycomptech.com>
2024-09-20 00:46:21 +0530

    added routing for products component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { DecorComponent } from './decor/decor.component';
+import { ProductsComponent } from './decor/products/products.component';
 
 export const routes: Routes = [
   {
     path: 'decor',
     component: DecorComponent
+  },
+  {
+    path: 'decor/:type',
+    component: ProductsComponent
   },
   {
     path: '',
--- src/app/decor/main-products/main-products.component.ts
+++ src/app/decor/main-products/main-products.component.ts
@@ -4,9 +4,10 @@ import { Observable } from 'rxjs';
 import { DecorCategories } from '../../models/decor-model';
 import { AsyncPipe } from '@angular/common';
 import { error } from 'console';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-main-products',
+  selector: 'main-products',
   standalone: true,
   imports: [AsyncPipe],
   templateUrl: './main-products.component.html',
@@ -17,9 +18,13 @@ export class MainProductsComponent implements OnInit {
   mainCategories?: Observable<DecorCategories[]>;
   // errorMessage: string | null = null; // comes from interceptor, need to add error component.
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(private categoryService: CategoryService, private router: Router) { }
 
   ngOnInit(): void {
     this.mainCategories = this.categoryService.getCategories()
   };
+
+  productNavigate(type: string) {
+    this.router.navigate(['/decor', type]);
+  }
 };
