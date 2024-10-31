43a71a0d4d3ea6217a275076803c498d70f13fa9
mohammed.umer <mohammed.umer@ivycomptech.com>
2024-10-31 20:30:27 +0530

    configured ngrx and effects lib

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,11 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { httpErrorInterceptor } from './http-interceptors';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-  provideClientHydration(),
-  provideHttpClient(withFetch(), withInterceptors([httpErrorInterceptor]))
-  ]
+    provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([httpErrorInterceptor])), provideStore(), provideEffects()]
 };
