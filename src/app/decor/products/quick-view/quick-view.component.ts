68438b41bb0e89ad7383451a9f25b5715b843c4b
mohammed.umer <mohammed.umer@ivycomptech.com>
2025-01-08 06:58:37 +0530

    popup close issue fixes

--- src/app/decor/products/quick-view/quick-view.component.ts
+++ src/app/decor/products/quick-view/quick-view.component.ts
@@ -1,4 +1,4 @@
-import { ElementRef, Renderer2 } from '@angular/core';
+import { HostBinding } from '@angular/core';
 import { Component } from '@angular/core';
 import { Products } from '../../../models/products.model';
 import { Store } from '@ngrx/store';
@@ -12,13 +12,13 @@ import * as CartActions from '../../../state/cart.actions';
   styleUrl: './quick-view.component.scss',
 })
 export class QuickViewComponent {
-  isVisible = false;
   product: Products = {} as Products;
   imgPath = 'assets/close.jpg';
 
   isItemAdded = false;
+  @HostBinding('class.popup-opened') isVisible = false;
 
-  constructor(private el: ElementRef, private renderer: Renderer2, private store: Store<{ item: IProductState }>) { }
+  constructor(private store: Store<{ item: IProductState }>) { }
 
   open(product: Products) {
     this.isVisible = true;
@@ -26,7 +26,8 @@ export class QuickViewComponent {
   }
 
   close() {
-    this.isVisible = false
+    this.isVisible = !this.isVisible;
+    this.isItemAdded = this.isVisible;
   }
 
   addItem(product: Products): void {
