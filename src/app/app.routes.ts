ddccbf1817ea0943fdbf0ac48ba4a9ee4c3b563d
umarshk-mb <2ab18cs019@anjuman.edu.in>
2025-07-28 00:18:04 +0530

    design update kiya yaro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { DecorComponent } from './decor/decor.component';
 import { ProductsComponent } from './decor/products/products.component';
 import { CartComponent } from './cart/cart.component';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { AboutComponent } from './about/about.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 export const routes: Routes = [
   {
@@ -20,6 +22,14 @@ export const routes: Routes = [
   {
     path: 'login',
     component: UserLoginComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent
   },
   {
     path: '',
